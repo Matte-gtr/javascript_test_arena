d8b5a126780c11ad9fff420a641996b5f6cec643
Matt <matt.snell.00@hotmail.co.uk>
2020-03-30 23:08:50 +0000

    added power, strict & sound event listeners

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-let power = "power";
-let strict = "strict";
-let sound = "sound";
-
-function powerCheck(string) {
-    let trget = "#" + string + "-button-container";
-    console.log(trget);
-    if ($(trget).css("backgroundColor") == "rgb(211, 211, 211)") {
-        value = false;
-        console.log(value);
-    } else {
-        value = true;
-        console.log(value);
-    };
-    return value;
-};
+let power = false;
+let strict = false;
+let sound = true;
 
 function buttonPower(el) {
     let prnt = $(el).parent();
@@ -27,9 +14,64 @@ function buttonPower(el) {
     };
 };
 
-function callResult() {
+$("#power-button").click(function() {
+    if($("#power-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
+        console.log("Power on");
+        power = true;
+    } else {
+        console.log("Power off");
+        power = false;
+    };
+});
+
+$("#strict-button").click(function() {
+    if($("#strict-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
+        console.log("strict on");
+        strict = true;
+    } else {
+        console.log("strict off");
+        strict = false;
+    };
+});
+
+$("#sound-button").click(function() {
+    if($("#sound-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
+        console.log("sound on");
+        sound = true;
+    } else {
+        console.log("sound off");
+        sound = false;
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function powerCheck(string) {
+    let trget = "#" + string + "-button-container";
+    console.log(trget);
+    if ($(trget).css("backgroundColor") == "rgb(211, 211, 211)") {
+        value = false;
+        console.log(value);
+    } else {
+        value = true;
+        console.log(value);
+    };
+    return value;
+};*/
+
+/*function callResult() {
     if (powerCheck(power) == true) {
         console.log("TRUE");
     } else
     console.log("FALSE");
-};
+};*/
