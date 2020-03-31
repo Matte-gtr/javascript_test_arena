7cbac6eb0bfdfd420ed3fb4383a4181148b13a31
Matt <matt.snell.00@hotmail.co.uk>
2020-03-31 22:54:02 +0000

    added difficulty button, console and play button with limited functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let power = false;
 let strict = false;
 let sound = true;
+let sequence = [];
+let playerSequence = [];
+let difficulty = 4;
+
+const topLeft = $("#section-1");
+const topRight = $("#section-2");
+const bottomLeft = $("#section-3");
+const bottomRight = $("#section-4");
+const middle = $("#section-5");
 
 function buttonPower(el) {
     let prnt = $(el).parent();
@@ -14,64 +23,90 @@ function buttonPower(el) {
     };
 };
 
-$("#power-button").click(function() {
-    if($("#power-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
-        console.log("Power on");
+$("#power-button").click(function () {
+    if ($("#power-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
         power = true;
+        $(".console").html("Click Play to Start");
     } else {
-        console.log("Power off");
         power = false;
+        $(".console").html("");
+        sequence = [];
+        $(".play-button").html("Play");
     };
 });
 
-$("#strict-button").click(function() {
-    if($("#strict-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
-        console.log("strict on");
+$("#strict-button").click(function () {
+    if ($("#strict-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
         strict = true;
     } else {
-        console.log("strict off");
         strict = false;
     };
 });
 
-$("#sound-button").click(function() {
-    if($("#sound-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
-        console.log("sound on");
+$("#sound-button").click(function () {
+    if ($("#sound-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
         sound = true;
     } else {
-        console.log("sound off");
         sound = false;
     };
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*function powerCheck(string) {
-    let trget = "#" + string + "-button-container";
-    console.log(trget);
-    if ($(trget).css("backgroundColor") == "rgb(211, 211, 211)") {
-        value = false;
-        console.log(value);
+$("#hard-button").click(function () {
+    if ($("#hard-button-container").css("backgroundColor") == "rgb(211, 211, 211)") {
+        difficulty = 5;
     } else {
-        value = true;
-        console.log(value);
+        difficulty = 4;
     };
-    return value;
-};*/
+});
 
-/*function callResult() {
-    if (powerCheck(power) == true) {
-        console.log("TRUE");
-    } else
-    console.log("FALSE");
-};*/
+window.addEventListener("keydown", function (key) {
+    if (key.keyCode == 17) {
+        $("#hard-display").removeClass("hidden-button");
+    };
+});
+
+window.addEventListener("keyup", function (key) {
+    if (key.keyCode == 17) {
+        $("#hard-display").addClass("hidden-button");
+    };
+});
+
+function randomNumber(max) {
+    newNum = Math.floor(Math.random() * Math.floor(max));
+    if (newNum == 0) {
+        return topLeft;
+    } else if (newNum == 1) {
+        return topRight;
+    } else if (newNum == 2) {
+        return bottomLeft;
+    } else if (newNum == 3) {
+        return bottomRight;
+    } else if (newNum == 4) {
+        return middle;
+    };
+};
+
+function startGame() {
+    if (power == true) {
+        sequence = [];
+        $(".play-button").html("Reset");
+        let level = sequence.length + 1;
+        $(".console").html(level);
+        sequence.push(randomNumber(difficulty));
+        playGame()
+    };
+};
+
+function playGame() {
+    for (let light of sequence) {
+        light.child().addClass("active")
+    };
+};
+
+
+
+
+
+
+
+
